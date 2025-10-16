523184e3ab79fcedc6cf42fd6bdfeb8ddbc8baeb
Samuel Wang <wang20sam@gmail.com>
2025-10-15 23:34:08 -0700

    Short edit fixed a warning in Arcade Drive

--- frc-training2025/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ frc-training2025/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.constants.IOConstants;
 import frc.robot.constants.MotorConstants;
 import frc.robot.subsystems.Drive;
 
