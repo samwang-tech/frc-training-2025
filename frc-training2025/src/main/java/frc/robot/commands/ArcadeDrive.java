b7114d993a48b5a7040d456c9bdff532ffafd69f
Samuel Wang <wang20sam@gmail.com>
2025-10-15 23:40:45 -0700

    Added to end method in command forgot about it before

--- frc-training2025/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ frc-training2025/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -55,7 +55,10 @@ public class ArcadeDrive extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_drive.setLeftSpeed(0);
+    m_drive.setRightSpeed(0);
+  }
 
   // Returns true when the command should end.
   @Override
